883d1cc9a4da53f90739c1735cd32a17e6b789ef
freelanceRG <83164618+freelanceRG@users.noreply.github.com>
2021-09-09 01:36:53 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.form__type').slick({
+		slidesToShow: 2,
+		slidesToScroll: 2,
+	});
+
+	$('.chipsets__slider').slick({
+		fade: true,
+		speed: 300,
+		cssEase: 'linear',
+		dots: true,
+	});
+	
+});
+
+	
